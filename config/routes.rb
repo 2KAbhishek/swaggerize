1f44b0f57930a5e414cf1c1aa43419cd28a887f4
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-02-27 14:23:37 +0530

    feat: generate user scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20230227085247_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :avatar
+      t.string :bio
+
+      t.timestamps
+    end
+  end
+end
