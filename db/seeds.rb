931be3edefb9f5ae52f5772173684443e6c1c38b
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-02-27 14:58:36 +0530

    feat: add post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,28 @@ num.times do
     name: Faker::Name.name,
     password: Faker::Internet.password,
     avatar: Faker::Avatar.image,
-    profession: Faker::Job.title,
     bio: Faker::Company.catch_phrase
   )
 end
 
 users = User.all
 puts("Seeded #{users.count} users!")
+
+users.each do |user|
+  Post.create!(
+    [
+      {
+        user_id: user.id,
+        title: Faker::Job.field,
+        content: Faker::Hacker.say_something_smart
+      },
+      {
+        user_id: user.id,
+        title: Faker::Job.field,
+        content: Faker::Hacker.say_something_smart
+      }
+    ]
+  )
+end
+
+puts("Seeded #{Post.all.count} posts!")
